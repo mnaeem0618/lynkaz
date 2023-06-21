e3dd47b412b9560750f9cd4715b543c3abd77a04
Naeem <naeem@herosolutions.com.pk>
2023-06-21 15:36:31 +0500

    Chngings

--- components/layout.js
+++ components/layout.js
@@ -4,22 +4,24 @@ import Footer from "./footer";
 import { useRouter } from "next/router";
 export default function Layout({ children, siteSettings }) {
   const router = useRouter();
+
   const path = router.pathname;
   if (path != "/login" && path != "/signup") {
     return (
       <div className="content">
         <SiteMaster />
         <Header siteSettings={siteSettings} />
+
         {children}
         <Footer siteSettings={siteSettings} />
       </div>
-    )
+    );
   } else {
     return (
       <div className="content">
         <SiteMaster />
         {children}
       </div>
-    )
+    );
   }
 }

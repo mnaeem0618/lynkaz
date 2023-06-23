8c2496905366c9d3924a37be94d250c87f863779
Naeem <naeem@herosolutions.com.pk>
2023-06-23 14:21:45 +0500

    Video issues resolve

--- components/Image_video.js
+++ components/Image_video.js
@@ -1,80 +1,73 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Text from "@/components/text";
 import Image from "next/image";
 import { cmsFileUrl, getFileExtension } from "@/helpers/helper";
 
-export default function Image_video({mp4_file_name, webm_file_name, file_loc='images', poster, img_width = '670',img_height = '447', vid_width = '1880',vid_height = '1224' }){
+export default function Image_video({
+  mp4_file_name,
+  webm_file_name,
+  file_loc = "images",
+  poster,
+  img_width = "670",
+  img_height = "447",
+  vid_width = "1880",
+  vid_height = "1224",
+}) {
+  const videoRef = useRef();
+  let ext = getFileExtension(mp4_file_name);
+  const expression = /(Mac|iPhone|iPod|iPad)/i;
+  const [platform, setPlatform] = useState();
+  useEffect(() => {
+    setPlatform(navigator.platform);
+  });
 
-    let ext = getFileExtension(mp4_file_name);
-    const expression = /(Mac|iPhone|iPod|iPad)/i;
-
-    const [platform, setPlatform] = useState();
-    useEffect(() => {
-
-      console.log(navigator.platform);
-      setPlatform(navigator.platform)
-    });
-
-
-    return (
-        <>
-            {ext == "mp4" || ext == "webm" || ext == "mov" ? (
-                    <video
-                      className="CuAnimation_video__70Pvw"
-                      height={vid_height}
-                      width={vid_width}
-                      autoPlay={true}
-                      muted={true}
-                      loop={true}
-                      playsInline={true}
-                      poster={poster}
-                      preload="auto"
-                    >
+  useEffect(() => {    
+    videoRef.current?.load();
+  }, [mp4_file_name, webm_file_name]);
 
 
-{ (expression.test(platform)) ? 
+  return (
+    <>
+      {ext == "mp4" || ext == "webm" || ext == "mov" ? (
+        <video
+          className="CuAnimation_video__70Pvw"
+          height={vid_height}
+          width={vid_width}
+          autoPlay={true}
+          muted={true}
+          loop={true}
+          playsInline={true}
+          poster={poster}
+          preload="auto"
+          ref={videoRef}
+        >
+          {expression.test(platform) ? (
+            <source
+              type="video/mp4"
+              src={cmsFileUrl(mp4_file_name, file_loc)}
+              // data-src={cmsFileUrl(mp4_file_name, file_loc)}
+            />
+          ) : (
+            <source
+              type="video/webm"
+              src={cmsFileUrl(webm_file_name, file_loc)}
+              // data-src={cmsFileUrl(webm_file_name, file_loc)}
+            />
+          )}
 
-
-  <source
-                        type="video/mp4"
-                        src={cmsFileUrl(mp4_file_name, file_loc)}
-                        data-src={cmsFileUrl(mp4_file_name, file_loc)}
-                      />
-                      : 
-                      <source
-                        type="video/webm"
-                        src={cmsFileUrl(webm_file_name, file_loc)}
-                        data-src={cmsFileUrl(webm_file_name, file_loc)}
-                      /> 
-                     }
-
-                      
-
-                      {/* <source
-                        type="video/webm"
-                        src={cmsFileUrl(mp4_file_name, file_loc)}
-                        data-src={cmsFileUrl(mp4_file_name, file_loc)}
-                      /> */}
-
-
-                      
-                    </video>
-                  ) : (
-                    <Image
-                      src={cmsFileUrl(mp4_file_name, file_loc)}
-                      width={img_width}
-                      height={img_height}
-                      alt="banner"
-                    />
-                  )}
-
-
-
-
-
-        </>
-    )
+      
+        </video>
+      ) : (
+        <Image
+          src={cmsFileUrl(mp4_file_name, file_loc)}
+          width={img_width}
+          height={img_height}
+          alt="banner"
+        />
+      )}
+    </>
+  );
 }
